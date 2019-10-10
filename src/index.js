acc33fe44060db193ae16c42ae290265f5d677d0
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-10-10 16:17:53 -0700

    finished form validation for title and description of streams/new route. Working on serverside api routes now

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware())
+    composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
