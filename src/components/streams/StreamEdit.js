dff6fa084842389ab7bfcaf59c5d315f9a8dc255
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-10-15 23:26:43 -0700

    initial commit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions'
 
-const StreamEdit = (props) => {
-    console.log(props);
-    return (
-        <div>
-            
-        </div>
-    )
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams(this.props.match.params.id);
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                StreamEdit
+            </div>
+        )
+    }
 }
 
 
 // ownProps displays the props presented to the component
 const mapStateToProps = (state, ownProps) => {
-    console.log(ownProps);
+    // console.log(ownProps);
     return { streams: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps)(StreamEdit)
+export default connect(mapStateToProps, { fetchStreams })(StreamEdit)
