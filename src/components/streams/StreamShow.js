5391574b00881d9bf57b5ffc7ddab2a6926a5a8a
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-10-18 16:31:14 -0700

    OBS streaming connection finished. Overall proj is completed. Will start on creating a solid readME now. Then come back periodically to improve CSS design on app

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef()
+    }
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    // builds player incase the component is ever updated
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    // cleans up some console.log
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    // checks if the player is already set up or the stream does not exist
+    // or else, it will set up the player
+    buildPlayer() {
+        if(this.player || !this.props.stream) {
+            return;
+        }
+
+        // boilerplate code from flv package to fetch the stream
+        this.player = flv.createPlayer({
+            type:'flv',
+            url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
@@ -17,6 +50,7 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
